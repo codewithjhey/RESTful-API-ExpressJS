40b2a246f23923cea9a95add9050b2f72465653c
codewithjhey <114162135+codewithjhey@users.noreply.github.com>
2022-12-20 15:54:42 +0000

    exercises completed expect extras

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from "express"
+import listEndpoints from "express-list-endpoints"
+import authorsRouter from "./api/authors/index.js"
+
+const server = express()
+
+const port = 3001
+
+server.use(express.json())
+
+server.use("/authors", authorsRouter)
+
+server.listen(port, () => {
+  console.table(listEndpoints(server))
+  console.log("server is running on port:", port)
+})
